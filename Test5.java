63629b97859ff28f2739fecd7fce56f03d988376
Alexander-Pinchuk <PinchukC@mail.ru>
2022-10-05 09:38:01 +0300

    Second commit

---
+++ Test5.java
@@ -0,0 +1,11 @@
+package AAAAA;
+
+import java.util.Arrays;
+
+public class Test5 {
+    public static void main(String[] args) {
+        int [] array = {10,4,76,1,2,3,4,5};
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
